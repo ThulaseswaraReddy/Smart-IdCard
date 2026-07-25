fb7bf09a08423eed7be52f1c6db256791ab4ec49
ThulaseswaraReddy <tulasireddy141149@gmail.com>
2026-07-25 19:29:22 +0530

    Initial commit for production deployment

--- db.py
+++ db.py
@@ -1,10 +1,27 @@
-
+import os
 import mysql.connector
-connection=mysql.connector.connect(
-    host="mysql.railway.internal",
-    port=3306,
-    user="root",
-    password="IOOvrJIozRKwYOTljpYXOzPJNkiyslYv",
-    database="railway"
+
+# Reads cloud environment variables when deployed, or uses your local credentials as fallback
+connection = mysql.connector.connect(
+    host=os.environ.get("DB_HOST", "localhost"),
+    port=int(os.environ.get("DB_PORT", 3306)),
+    user=os.environ.get("DB_USER", "root"),
+    password=os.environ.get("DB_PASSWORD", "6264"),
+    database=os.environ.get("DB_NAME", "smartidcard"),
+    ssl_disabled=(os.environ.get("DB_HOST") is None)
 )
-cursor=connection.cursor(dictionary=True)
+
+cursor = connection.cursor(dictionary=True)
+
+
+
+
+# import mysql.connector
+# connection=mysql.connector.connect(
+#     host="localhost",
+#     port=3306,
+#     user="root",
+#     password="6264",
+#     database="smartidcard"
+# )
+# cursor=connection.cursor(dictionary=True)
